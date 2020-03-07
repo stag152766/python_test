36ad8f1a0ce2c17e77f4f761c97476e00d7f8d62
stag152766 <45874547+stag152766@users.noreply.github.com>
2020-03-07 18:29:06 +0700

    lesson4
    further refactor

--- test_add_group.py
+++ test_add_group.py
@@ -11,12 +11,21 @@ class UntitledTestCase(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-    def test_untitled_test_case(self):
+    def test_add_group(self):
         driver = self.driver
         self.open_home_page(driver)
-        self.login(driver)
+        self.login(driver, username="admin", password="secret")
         self.open_group_page(driver)
-        self.create_group(driver)
+        self.create_group(driver, name="asdfsaf", header="sdfsadf", footer="sadfsdaf")
+        self.return_to_group_page(driver)
+        self.logout(driver)
+
+    def test_add_empty_group(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_group_page(driver)
+        self.create_group(driver, name="", header="", footer="")
         self.return_to_group_page(driver)
         self.logout(driver)
 
@@ -26,31 +35,31 @@ class UntitledTestCase(unittest.TestCase):
     def return_to_group_page(self, driver):
         driver.find_element_by_link_text("groups").click()
 
-    def create_group(self, driver):
+    def create_group(self, driver, name, header, footer):
         # init group creation
         driver.find_element_by_name("new").click()
         # fill group form
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("asdfsaf")
+        driver.find_element_by_name("group_name").send_keys(name)
         driver.find_element_by_name("group_header").click()
         driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("sdfsadf")
+        driver.find_element_by_name("group_header").send_keys(header)
         driver.find_element_by_name("group_footer").click()
         driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("sadfsdaf")
+        driver.find_element_by_name("group_footer").send_keys(footer)
         # submit group creation
         driver.find_element_by_name("submit").click()
 
     def open_group_page(self, driver):
         driver.find_element_by_link_text("groups").click()
 
-    def login(self, driver):
+    def login(self, driver, username, password):
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, driver):
