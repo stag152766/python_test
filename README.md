a4917a6e8cd234de54748dbfa4764c5d54c09cda
stag152766 <45874547+stag152766@users.noreply.github.com>
2020-03-07 17:55:20 +0700

    lesson3
    refactoring test_add_group

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # python_test
  
+
+pip install virtualenv 
+
+cd / корень проекта
+
+создание вирутального окружения
+virtualenv env 
+
+активация  
+env\Scripts\activate
+
+установка в виртуальное окружение
+pip install seleninum
+pip install pytest
+
+обновление chromedriver 
+https://sites.google.com/a/chromium.org/chromedriver/downloads
+
+переключение тест раннера
+settings > tools > Python Integreted Tools > Default Test Runner
+
--- test_add_group.py
+++ test_add_group.py
@@ -1,31 +1,35 @@
 # -*- coding: utf-8 -*-
+import unittest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
         self.base_url = "https://www.google.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
-    
+
     def test_untitled_test_case(self):
         driver = self.driver
-        driver.get("http://localhost:81/addressbook/")
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
+        self.open_home_page(driver)
+        self.login(driver)
+        self.open_group_page(driver)
+        self.create_group(driver)
+        self.return_to_group_page(driver)
+        self.logout(driver)
+
+    def logout(self, driver):
+        driver.find_element_by_link_text("Logout").click()
+
+    def return_to_group_page(self, driver):
         driver.find_element_by_link_text("groups").click()
+
+    def create_group(self, driver):
+        # init group creation
         driver.find_element_by_name("new").click()
+        # fill group form
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
         driver.find_element_by_name("group_name").send_keys("asdfsaf")
@@ -35,34 +39,27 @@ class UntitledTestCase(unittest.TestCase):
         driver.find_element_by_name("group_footer").click()
         driver.find_element_by_name("group_footer").clear()
         driver.find_element_by_name("group_footer").send_keys("sadfsdaf")
+        # submit group creation
         driver.find_element_by_name("submit").click()
+
+    def open_group_page(self, driver):
         driver.find_element_by_link_text("groups").click()
-        driver.find_element_by_link_text("Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
+
+    def login(self, driver):
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, driver):
+        driver.get("http://localhost:81/addressbook/")
+
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
+
 
 if __name__ == "__main__":
     unittest.main()
