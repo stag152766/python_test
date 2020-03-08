6db2fddd6276fed73d3c26c3d901b9c33d6de4a9
stag152766 <45874547+stag152766@users.noreply.github.com>
2020-03-08 15:03:30 +0700

    lesson9
    Выделен класс для работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
 
 class Application:
@@ -8,36 +9,9 @@ class Application:
         self.driver.implicitly_wait(30)
         # конструирование помощника
         # помощник получает ссылку на саму фикстуру (объект класса Application)
-        # дает возможность в одном помощнике обратиться к другому, через объект класса Application
+        # это дает возможность в одном помощнике обратиться к другому, через объект класса Application
         self.session = SessionHelper(self)
-
-
-    def return_to_group_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        driver = self.driver
-        self.open_groups_page()
-        # init group creation
-        driver.find_element_by_name("new").click()
-        # fill group form
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys(group.name)
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(group.header)
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        driver.find_element_by_name("submit").click()
-        self.return_to_group_page()
-
-    def open_groups_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("groups").click()
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         driver = self.driver
