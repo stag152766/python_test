11ba33b794f5b69441f4d1b6f9883287a99b9d82
stag152766 <45874547+stag152766@users.noreply.github.com>
2020-04-29 01:06:01 +0700

    add asserts len list to group tests

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,5 @@
+from model.group import Group
+
 class GroupHelper:
     def __init__(self, app):
         self.app = app
@@ -56,3 +58,13 @@ class GroupHelper:
         driver = self.app.driver
         self.open_groups_page()
         return len(driver.find_elements_by_name("selected[]"))
+
+    def get_group_list(self):
+        driver = self.app.driver
+        self.open_groups_page()
+        groups = []
+        for element in driver.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name('selected[]').get_attribute('value')
+            groups.append(Group(name = text, id = id))
+        return groups
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,8 +2,14 @@ from model.group import Group
 
 
 def test_modify_first_group_name(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="New name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_first_group_header(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
