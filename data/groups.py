0d255ccc350f827f07490d09dfce2ef4bf0113dc
stag152766 <45874547+stag152766@users.noreply.github.com>
2020-05-08 19:40:59 +0700

    add gererator testdata

--- data/groups.py
+++ data/groups.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-import pytest
-import random
-import string
 
 
-constant = (Group(name='name1', header='header1', footer='footer1'),
+
+testdata = (Group(name='name1', header='header1', footer='footer1'),
             Group(name='name2', header='header2', footer='footer2'))
 
