15e0443dd42367ef2a8bc88112e2b81678af9e68
stag152766 <45874547+stag152766@users.noreply.github.com>
2020-05-15 00:39:53 +0700

    add get_contacts_from_group

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,10 +1,11 @@
 from fixture.orm import OrmFixture
+from model.group import Group
 
 
 db = OrmFixture(host='127.0.0.1', database='addressbook', user='root', password='')
 
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_not_in_group(Group(id='153'))
     for item in l:
         print(item)
     print(len(l))
--- fixture/orm.py
+++ fixture/orm.py
@@ -13,6 +13,7 @@ class OrmFixture:
         name = Optional(str, column='group_name')
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
+        contacts = Set(lambda: OrmFixture.ORMContact, table="address_in_groups", column='id', reverse='groups', lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
@@ -20,6 +21,7 @@ class OrmFixture:
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
         deprecated = Optional(datetime, column='deprecated')
+        groups = Set(lambda: OrmFixture.ORMGroup, table="address_in_groups", column='group_id', reverse='contacts', lazy=True)
 
     def __init__(self, host, database, user, password):
         self.db.bind('mysql', host=host, database=database, user=user, password=password)
@@ -45,6 +47,13 @@ class OrmFixture:
     def get_contact_list(self):
         return self.convert_contacts_to_model(select(c for c in OrmFixture.ORMContact if c.deprecated is None))
 
-    #test_git
+    @db_session
+    def get_contacts_from_group(self, group):
+        orm_group = list(select(g for g in OrmFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(orm_group.contacts)
 
-
+    @db_session
+    def get_contacts_not_in_group(self, group):
+        orm_group = list(select(g for g in OrmFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(
+            select(c for c in OrmFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
